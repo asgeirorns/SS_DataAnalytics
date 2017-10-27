2aabdef233448fc8f4031b1496df29a38d99a721
Ásgeir Örn Sigurpálsson <asgeirorns@gmail.com>
2017-10-27 23:27:11 +0000

    Update CaseMix.py

--- CaseMix.py
+++ CaseMix.py
@@ -1,18 +1,22 @@
 #Author: Asgeir Orn Sigurpalsson
+#Date: 27/10/2017 23:24
+#---------------------------------------------------------------------#
 #Case mix classification - based on the article:
     # Case mix classification and benchmark set for surgery scheduling
     #by Greanne Leeftink and Erwin W. Hans (2017)
-#----------------------------------------------------------------------------#
+
+#Next steps: Clean the data - gives wrong results for some cases
+#        +Some data is missing
+#--------------------------------------------------------------------#
 import pandas as pd
 import numpy as np
 import math
 import matplotlib.pyplot as plt
 from scipy import stats
-fname='___.csv'
+import xlsxwriter
+fname='-----.csv'
 df = pd.read_csv(fname, delimiter=";", encoding="utf_8", header=0,
         skiprows=range(0,1),skip_blank_lines=True,parse_dates=True)
-
-#print(df)
 
 #dataframe
 dfOperation = pd.DataFrame(df, columns=['Code of surg. case','Time of operation'])
@@ -23,21 +27,28 @@ group=group['Time of operation'].agg([np.mean,  np.std]).reset_index()
 group = group.fillna(0)
 #print(group)
 #-----------------------------------------------------------------#
-#According to May et al.,2000 and Stephaniak et al. 2009 then
+
 #3-parameter lognormal distribution have the best fit with surgery
 #Duration distributions.
+    #We are missing the location parameter for the mean...
 group['mt']=  np.exp((group['mean']/60)+((((group['std']/60)**2)/2)))
-#We are missing the location parameter for the mean...
-calc_std= ((np.exp((group['std']/60)**2))-1)*np.exp((2*group['mean']/60)+(group['std']/60))
-group['st']=calc_std**(1/2)
 
-#lets define the load parameter
+#Stdev
+group['st']= (((np.exp((group['std']/60)**2))-1)*np.exp((2*(group['mean']/60))+((group['std']/60)**2)))**(1/2)
+
+#lets define the load parameter - must check this better
 c=8*1.2*4
-
+#Add extra columns to the matrix to analyse
+group['xas']=group['mt']/c
+group['yas']=group['st']/group['mt']
 plt.scatter(group['mt']/c, group['st']/group['mt'])
-print(group)
+
 plt.xlim((0,1))
-plt.ylim((0,2))
-
-
+plt.ylim((0,1))
 plt.show()
+# Create a Pandas Excel writer using XlsxWriter as the engine.
+writer = pd.ExcelWriter('analytics.xlsx', engine='xlsxwriter')
+# Convert the dataframe to an XlsxWriter Excel object.
+group.to_excel(writer, sheet_name='Sheet1')
+# Close the Pandas Excel writer and output the Excel file.
+writer.save()
